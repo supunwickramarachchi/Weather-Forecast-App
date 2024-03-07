48e5ab5b5dcafa6a223b67d6f5b1c8d5a8cc2e94
SpaceSeeker101 <86275338+SpaceSeeker101@users.noreply.github.com>
2024-03-07 13:46:02 +0530

    Day 31: Plot face data dynamically

--- main.py
+++ main.py
@@ -11,4 +11,14 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
+def get_data(select_date):
+    dates = ["2024-03-15", "2024-03-16", "2024-03-17", "2024-03-18"]
+    temperature = [20, 22, 25, 31]
+    temperature = [days * i for i in temperature]
+    return dates, temperature
 
+
+date_list, temperature_list = get_data(days)
+
+figure = px.line(x=date_list, y=temperature_list, labels={"x": "Dates", "y": "Temperature (C)"})
+st.plotly_chart(figure)
